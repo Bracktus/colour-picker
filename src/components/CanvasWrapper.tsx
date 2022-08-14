c749679550f8a89ce9183e289af3842d91280453
Bracktus <specialistricky@gmail.com>
2022-08-14 14:18:31 +0100

    change size of preview

--- src/components/CanvasWrapper.tsx
+++ src/components/CanvasWrapper.tsx
@@ -9,11 +9,9 @@ interface CanvasWrapperProps {
 }
 
 export const CanvasWrapper: React.FC<CanvasWrapperProps> = ({ colours, sketches }) => {
-  const width = 800;
-  const height = 800;
+  const width = 500;
+  const height = 500;
 
-  //Looks confusing but it's how to store a function in state
-  // https://dev.to/sarioglu/how-to-store-a-function-using-react-usestate-4ffh
   const [funcName, setFuncName] = useState("Default");
   const drawFunc = Object.values(sketches).flat().find(sketch => funcName === sketch.name).func;
 
